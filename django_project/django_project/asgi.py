e13458974255bda0787ac9237579a8927d41e7ba
ajgreenwell <greenwan@bc.edu>
2020-02-09 20:31:02 -0500

    initializing project

--- django_project/django_project/asgi.py
+++ django_project/django_project/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for ta_system project.
+ASGI config for django_project project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ta_system.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_project.settings')
 
 application = get_asgi_application()
---
+++ django_project/ta_system/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TaSystemConfig(AppConfig):
+    name = 'ta_system'
