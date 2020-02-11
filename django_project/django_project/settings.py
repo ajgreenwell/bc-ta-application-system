b3a151831cf13744cd1c0f6f6ba6307155d50c0c
ajgreenwell <greenwan@bc.edu>
2020-02-11 13:58:23 -0500

    adding usrs app

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
     'ta_system.apps.TaSystemConfig',
     'django.contrib.admin',
     'django.contrib.auth',
