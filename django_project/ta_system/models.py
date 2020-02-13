e8c0d61f48f3be16b3f1ce21d4ba245ceb32a229
ajgreenwell <greenwan@bc.edu>
2020-02-12 21:54:41 -0500

    testing branching

--- django_project/ta_system/models.py
+++ django_project/ta_system/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Course(models.Model):
+    course_num =
+    section_num =
+    name = 
+    professor =
+    days_of_week =
+    start_time =
+    end_time =
+    building =
+    room = 
+    max_num_tas =
+
+    def full_course_number(self):
+        f'{self.course_num}{self.section_num}'
+
+    def __str__(self):
+        return f'{self.full_course_number()}: {self.name}'
