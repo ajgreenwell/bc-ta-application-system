5abd651067247850d950ca5fa4d85e452ee6f68a
ajgreenwell <greenwan@bc.edu>
2020-02-22 11:50:42 -0500

    creating student data upload form

--- django_project/ta_system/forms.py
+++ django_project/ta_system/forms.py
@@ -1,4 +1,9 @@
 from django import forms
 
+
 class CourseDataUploadForm(forms.Form):
     file = forms.FileField(label='Course Data CSV')
+
+
+class StudentDataUploadForm(forms.Form):
+    file = forms.FileField(label='Student Data CSV')
