db66580af142a31aba3ecb11c36c65b46811de8a
Colin Clarke <colinclarke1010@gmail.com>
2020-02-23 19:47:31 -0500

    removed registration functionality

--- django_project/users/forms.py
+++ django_project/users/forms.py
@@ -2,17 +2,3 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
-
-class UserRegisterForm(UserCreationForm):
-    username = forms.EmailField(
-        label='BC Email'
-    )
-
-    eagleid = forms.IntegerField(
-        label="Eagle ID",
-        required=True,
-    )
-
-    class Meta:
-        model = User
-        fields = ['username', 'eagleid', 'password1', 'password2']
