0d342354fc451588140233fa4a64292f4aeed1fa
Anny Shan <annysp28@gmail.com>
2020-04-03 02:31:50 -0400

    postgressql updated

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -80,9 +80,18 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        # 'PASSWORD': 'ajg123',
+        'PASSWORD': '<3',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
